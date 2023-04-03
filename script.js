81ff956fd0ae56cf16f27f3b514b2b98b1ba405c
shiamsharif <shiam.sharif.07@gmail>
2023-04-03 19:30:36 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburgerWrapper = document.querySelector('hamburger_wrapper');
+
+hamburgerWrapper.addEventListener('click', () => {
+    hamburgerWrapper.classList.toggle('active');
+});
+console.log(hi)
